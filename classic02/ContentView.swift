a2bd50cffdecb858e8b3028e47a54867b71aa5fe
Dujiashan0711 <shandu7652@gmail.com>
2021-09-03 21:29:13 +0800

    增加text

---
+++ classic02/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  classic02
+//
+//  Created by 暨大附中２ on 2021/9/3.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack{
+        Text("學習如何Git版控")
+            .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+            .padding(.top,40)
+            Spacer()
+        Text("好好學會版控日後會方便很多")
+            .padding(.bottom,40)
+        }
+        .frame(height: 200, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+        .frame( maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+        .background(Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)))
+        .cornerRadius(16)
+        .padding()
+}
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ classic02/classic02App.swift
@@ -0,0 +1,17 @@
+//
+//  classic02App.swift
+//  classic02
+//
+//  Created by 暨大附中２ on 2021/9/3.
+//
+
+import SwiftUI
+
+@main
+struct classic02App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
