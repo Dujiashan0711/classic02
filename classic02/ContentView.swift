1cb1148f0e83aad55eba28d90bd34dfb4046bb70
Dujiashan0711 <shandu7652@gmail.com>
2021-09-04 15:20:04 +0800

    增加text以及改變框架排版

--- classic02/ContentView.swift
+++ classic02/ContentView.swift
@@ -10,15 +10,18 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack{
-        HStack{
+            Text("Shan.D - 28%完成度")
+                .font(.system(size:10))
+                .padding(.top,80)
+            HStack{
             Image(systemName: "gear")
                 .foregroundColor(Color(#colorLiteral(red: 0.5760982144, green: 0.5818219629, blue: 0.7321725399, alpha: 1)))
                 .font(.system(size: 20))
-                .padding(.top,120)
+                .padding(.top)
             Text("Account")
                 .bold()
                 .font(.none)
-                .padding(.top,120)
+                .padding(.top)
             }
             Spacer()
         HStack{
@@ -47,7 +50,7 @@ struct ContentView: View {
         }
         .frame(height: 250, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
         .frame( maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
-        .background(LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)), Color(#colorLiteral(red: 0.8181295521, green: 0.8553103241, blue: 0.9076588365, alpha: 1)),Color(#colorLiteral(red: 0.7369184916, green: 0.7613699301, blue: 0.8156720966, alpha: 1))]), startPoint: .top, endPoint: .bottom))
+        .background(LinearGradient(gradient: Gradient(colors:[Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)), Color(#colorLiteral(red: 0.9159325133, green: 0.9159325133, blue: 0.9159325133, alpha: 1)),Color(#colorLiteral(red: 0.8499833777, green: 0.8499833777, blue: 0.8499833777, alpha: 1))]), startPoint: .top, endPoint: .bottom))
         .cornerRadius(30)
         .shadow(color: Color.black.opacity(0.3), radius: 20, x: 0, y: 20)
         .padding()
