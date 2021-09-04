6de5d4bfe0c85bb1a6d41b6c75e46bc4f062fb6f
Dujiashan0711 <shandu7652@gmail.com>
2021-09-04 14:43:08 +0800

    放大圖像

--- classic02/ContentView.swift
+++ classic02/ContentView.swift
@@ -13,6 +13,7 @@ struct ContentView: View {
         HStack{
             Image(systemName: "gear")
                 .foregroundColor(Color(#colorLiteral(red: 0.5760982144, green: 0.5818219629, blue: 0.7321725399, alpha: 1)))
+                .font(.system(size: 20))
                 .padding(.top,120)
             Text("Account")
                 .bold()
@@ -23,6 +24,7 @@ struct ContentView: View {
         HStack{
             Image(systemName: "creditcard")
                 .foregroundColor(Color(#colorLiteral(red: 0.5760982144, green: 0.5818219629, blue: 0.7321725399, alpha: 1)))
+                .font(.system(size: 20))
                 .padding(.leading,15)
             Text("Billing")
                 .bold()
@@ -35,6 +37,7 @@ struct ContentView: View {
             Image(systemName:
                     "person.crop.circle")
                 .foregroundColor(Color(#colorLiteral(red: 0.5760982144, green: 0.5818219629, blue: 0.7321725399, alpha: 1)))
+                .font(.system(size: 20))
                 .padding(.bottom,30)
             Text("Sign out")
                 .bold()
@@ -44,7 +47,7 @@ struct ContentView: View {
         }
         .frame(height: 250, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
         .frame( maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
-        .background(LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)), Color(#colorLiteral(red: 0.9076588365, green: 0.9076588365, blue: 0.9076588365, alpha: 1)),Color(#colorLiteral(red: 0.8156720966, green: 0.8156720966, blue: 0.8156720966, alpha: 1))]), startPoint: .top, endPoint: .bottom))
+        .background(LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)), Color(#colorLiteral(red: 0.8181295521, green: 0.8553103241, blue: 0.9076588365, alpha: 1)),Color(#colorLiteral(red: 0.7369184916, green: 0.7613699301, blue: 0.8156720966, alpha: 1))]), startPoint: .top, endPoint: .bottom))
         .cornerRadius(30)
         .shadow(color: Color.black.opacity(0.3), radius: 20, x: 0, y: 20)
         .padding()
